c5402f5454096e0881e22c48f4664a91a45cd920
Dave Mendlen <davemen@hotmail.com>
2017-07-03 18:12:02 -0700

    Add files via upload
    Initial

---
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Collections.Generic;
+
+namespace Bot_Application1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+           
+            //// calculate something for us to return
+            //int length = (activity.Text ?? string.Empty).Length;
+
+            //// return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            var message = context.MakeMessage();
+
+            var heroCard = new HeroCard
+            {
+                Title = "Neil Leslie Says Build a Bot!",
+                Subtitle = "Your bots — you can do it.",
+                Text = "Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.",
+                Images = new List<CardImage> { new CardImage("https://i.ytimg.com/vi/BxzKHZAwFCM/maxresdefault.jpg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Get Started", value: "https://docs.microsoft.com/bot-framework") }
+            };
+
+            message.Attachments.Add(heroCard.ToAttachment());
+
+            await context.PostAsync(message);
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+    }
+}
